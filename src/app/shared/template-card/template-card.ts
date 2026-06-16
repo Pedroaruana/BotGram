d164479f7d0a145f7169980b14638d9e300ec2ea
Pedroaruana <aruanapedro@gmail.com>
2026-06-16 17:55:54 -0300

    componente de card para os templates

---
+++ src/app/shared/template-card/template-card.ts
@@ -0,0 +1,26 @@
+import { Component, input, output } from '@angular/core';
+
+export interface BotTemplate {
+  id: number;
+  icon: string;
+  name: string;
+  desc: string;
+  category: string;
+  free: boolean;
+  preview: string[];
+}
+
+@Component({
+  selector: 'app-template-card',
+  imports: [],
+  templateUrl: './template-card.html',
+  styleUrl: './template-card.scss',
+})
+export class TemplateCard {
+  template = input.required<BotTemplate>();
+  onSelect = output<BotTemplate>();
+
+  select() {
+    this.onSelect.emit(this.template());
+  }
+}
