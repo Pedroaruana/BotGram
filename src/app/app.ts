1428bef77c2c9513993e8e70bc9697e4648b58cd
Pedroaruana <aruanapedro@gmail.com>
2026-06-24 18:53:31 -0300

    footer component with nav links and credits

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   template: `
     <app-navbar />
     <router-outlet />
+    <app-footer />
   `,
   styleUrl: './app.scss',
 })
